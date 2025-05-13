8cd9f26b591f2396b43d8e6d7ada91ae26b1584a
ishita-anantacloud <ishita.kapoor@anantacloud.com>
2025-05-13 18:24:50 +0530

    Update file content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,91 +6,84 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-title: 'Ananta Cloud Docs',
+  title: 'Ananta Cloud Docs',
 
+  url: 'https://your-docusaurus-site.example.com',
+  baseUrl: '/',
 
-url: 'https://your-docusaurus-site.example.com',
-baseUrl: '/',
+  organizationName: 'anantacloud', // Replace with your GitHub org/user
+  projectName: 'Documentation Site', // Replace with your repo name
 
-organizationName: 'anantacloud', // Replace with your GitHub org/user
-projectName: 'Documentation Site', // Replace with your repo name
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
-onBrokenLinks: 'throw',
-onBrokenMarkdownLinks: 'warn',
-
-i18n: {
-defaultLocale: 'en',
-locales: ['en'],
-},
-
-presets: [
-[
-'classic',
-/** @type {import('@docusaurus/preset-classic').Options} */
-({
-docs: {
-id: 'default',
-path: 'docs',
-routeBasePath: 'docs',
-sidebarPath: require.resolve('./sidebars.js'),
-editUrl:
-'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-},
-
-editUrl:
-'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-},
-
-theme: {
-customCss: require.resolve('./src/css/custom.css'),
-},
-}),
-],
-],
-
-plugins: [
-[
-'@docusaurus/plugin-content-docs',
-{
-id: 'developer-docs',
-path: 'developer-docs',
-routeBasePath: 'developer-docs',
-sidebarPath: require.resolve('./sidebarsDeveloper.js'),
-editUrl:
-'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-},
-],
-],
-
-themeConfig: /** @type {import('@docusaurus/preset-classic').ThemeConfig} */ ({
-navbar: {
-logo: {
-alt: 'Ananta Cloud Logo',
-src: 'https://static.wixstatic.com/media/acdaa6_a907d2fb0e354e8fac207526c8e7609f~mv2.png/v1/crop/x_34,y_61,w_434,h_343/fill/w_98,h_76,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/logo2.png',
-},
-items: [
-{ to: '/docs/devops-foundations', label: 'Docs', position: 'left' },
-{ to: '/developer-docs/introduction', label: 'Reference', position: 'left' },
-{ to: '/signin', label: 'Sign In', position: 'right' },
-{ type: 'search', position: 'right' },
-],
-},
-
-
-prism: {
-  theme: prismThemes.github,
-  darkTheme: prismThemes.dracula,
-},
-
-
-colorMode: {
-    defaultMode: 'light',
-    disableSwitch: false,
-    respectPrefersColorScheme: true,
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          id: 'default',
+          path: 'docs',
+          routeBasePath: 'docs',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
 
-}),
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'developer-docs',
+        path: 'developer-docs',
+        routeBasePath: 'developer-docs',
+        sidebarPath: require.resolve('./sidebarsDeveloper.js'),
+        editUrl:
+          'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+      },
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        logo: {
+          alt: 'Ananta Cloud Logo',
+          src: 'https://static.wixstatic.com/media/acdaa6_a907d2fb0e354e8fac207526c8e7609f~mv2.png/v1/crop/x_34,y_61,w_434,h_343/fill/w_98,h_76,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/logo2.png',
+        },
+        items: [
+          { to: '/docs/devops-foundations', label: 'Docs', position: 'left' },
+          { to: '/developer-docs/introduction', label: 'Reference', position: 'left' },
+          { to: '/signin', label: 'Sign In', position: 'right' },
+          { type: 'search', position: 'right' },
+        ],
+      },
+
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
+    }),
 };
 
 export default config;
+
