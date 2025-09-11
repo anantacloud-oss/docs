a06f74ae32e0ce1f0ff44d8cd54cc1eb4cead138
ishita-anatacloud <ishita.kapoor@anantacloud.com>
2025-09-11 13:03:54 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,10 +1,11 @@
-const { themes: prismThemes } = require('prism-react-renderer');
+import { themes as prismThemes } from 'prism-react-renderer';
 
 const config = {
   title: 'Ananta Cloud Docs',
   tagline: "Discover how to manage and deploy infrastructure efficiently with infrastructure as code.",
   url: 'https://reimagined-adventure-lrry9rm.pages.github.io',
   baseUrl: '/',
+
   organizationName: 'anantacloud',
   projectName: 'docs',
   trailingSlash: false,
@@ -43,6 +44,7 @@ const config = {
         sidebarPath: require.resolve('./sidebarsDeveloper.js'),
       },
     ],
+     
     [
       '@docusaurus/plugin-content-docs',
       {
@@ -51,35 +53,38 @@ const config = {
         routeBasePath: 'modules-docs',
         sidebarPath: require.resolve('./sidebarmodule.js'),
       },
-    ],
-  ],
+   ],
+ ],
 
-  themeConfig: {
-    favicon: 'img/favicon.ico',
-    navbar: {
-      logo: {
-        alt: 'Ananta Cloud Logo',
-        src: 'img/logo.png',
-      },
-      items: [
-        { to: '/docs/getting-started', label: 'Documentations', position: 'left' },
-        { to: '/developer-docs/introduction', label: 'References', position: 'left' },
-        { to: '/modules-docs/searchmodule', label: 'Infrastructure as a Code', position: 'left' },
-        { to: 'https://github.com/orgs/anantacloud/discussions', label: 'Knowledge-Hub', position: 'right' },
-        { to: '/docs/Support', label: 'Support', position: 'right' },
-        {
-          type: 'html',
-          position: 'right',
-          value: '<a class="navbar__link custom-schedule-button" href="https://calendly.com/anantacloud/30min" target="_blank">Schedule Meeting</a>',
+  themeConfig:
+    ({
+
+     favicon: 'img/favicon.ico',     
+
+      navbar: {
+        logo: {
+          alt: 'Ananta Cloud Logo',
+          src:  'img/logo.png',
         },
-        { type: 'search', position: 'right' },
-      ],
-    },
-    colorMode: {
-      disableSwitch: true,
-    },
-  },
+        items: [
+          { to: '/docs/getting-started', label: 'Documentations', position: 'left' },
+          { to: '/developer-docs/introduction', label: 'References', position: 'left' },
+          { to: '/modules-docs/searchmodule', label: 'Infrastructure as a Code', position: 'left' },
+          { to: 'https://github.com/orgs/anantacloud/discussions', label: 'Knowledge-Hub', position: 'right' },
+          { to: '/docs/Support', label: 'Support', position: 'right' },
+          {
+             type: 'html',
+             position: 'right',
+             value: '<a class="navbar__link custom-schedule-button" href="https://calendly.com/anantacloud/30min" target="_blank">Schedule Meeting</a>',
+          },
+          { type: 'search', position: 'right' },
+        ],
+      },
+
+      colorMode: {
+          disableSwitch: true, 
+     },
+  }),
 };
 
-module.exports = config;
-
+export default config;      
