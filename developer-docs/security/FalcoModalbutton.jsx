036aefba261269b1f9839c2b595a56638b1e5993
ishita-anatacloud <ishita.kapoor@anantacloud.com>
2026-02-03 11:44:20 +0530

    Update FalcoModalbutton.jsx

--- developer-docs/security/FalcoModalbutton.jsx
+++ developer-docs/security/FalcoModalbutton.jsx
@@ -93,7 +93,7 @@ export default function Modalbutton() {
                 Cancel
               </button>
               <a
-                href="https://github.com/anantacloud/github-actions/tree/main/security/falco"
+                href="https://github.com/anantacloud/actions/tree/main/security/falco"
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{ width: isMobile ? '100%' : 'auto' }}
