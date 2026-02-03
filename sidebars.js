41de4deb4443ae7b94ff6088f89cbd9fe427bf92
ishita-anantacloud <ishita.kapoor@anantacloud.com>
2026-02-03 08:35:58 +0530

    Modified file sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -17,7 +17,7 @@ const sidebars = {
         'introduction/welcome',
         'introduction/how-to-use',
         'introduction/target-audience',
-        'introduction/support-guidelines'
+        'introduction/support-guidelines',
       ]
     },
     {
@@ -40,7 +40,6 @@ const sidebars = {
             'devops/cicd/source-control',
           ],
         },
-        'devops/monitoring-and-observability'
       ],
     },
     {
@@ -50,10 +49,7 @@ const sidebars = {
         'iac/overview',
         'iac/folder-structure',
         'iac/tagging-and-naming-conventions',
-        'iac/developer-self-service-patterns',
-        'iac/choosing-iac-platform',
         'iac/prebuilt-iac-modules',
-        'iac/testing-and-validation'
       ],
     },
     {
@@ -64,7 +60,7 @@ const sidebars = {
         'landing-zone/deployment-strategy',
         'landing-zone/deploying-to-aws',
         'landing-zone/deploying-to-gcp',
-        'landing-zone/deploying-to-azure'
+        'landing-zone/deploying-to-azure',
       ],
     },
     {
@@ -73,11 +69,7 @@ const sidebars = {
       items: [
         'security/overview',
         'security/cis-benchmark',
-        'security/compliance-architecture',
-        'security/audit-logging',
         'security/secret-and-key-management',
-        'security/security-left',
-        'security/iam'
       ],
     },
     {
