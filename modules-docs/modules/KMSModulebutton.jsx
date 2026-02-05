e6149aeaceb20841f65bff7b0de7279717f1ee51
ishita-anatacloud <ishita.kapoor@anantacloud.com>
2026-02-05 13:21:01 +0530

    Update KMSModulebutton.jsx

--- modules-docs/modules/KMSModulebutton.jsx
+++ modules-docs/modules/KMSModulebutton.jsx
@@ -89,7 +89,7 @@ export default function Modulebutton() {
             </h3>
             <p>
               {showModal === 'subscription'
-                ? `This link points to the private terraform-aws-key-management-module repository, which is accessible only to subscribers. Non-subscribers will receive a 404 error.`
+                ? `This link points to the private terraform-aws-kms-module repository, which is accessible only to subscribers. Non-subscribers will receive a 404 error.`
                 : `Here you can view the latest release notes, updates, and changes related to this module visible only to subscribers; everyone else will see a 404 error.`}
             </p>
             <div
@@ -121,8 +121,8 @@ export default function Modulebutton() {
               <a
                 href={
                   showModal === 'subscription'
-                    ? 'https://github.com/anantacloud/terraform-aws-key-management-module.git'
-                    : 'https://github.com/anantacloud/terraform-aws-key-management-module/releases'
+                    ? 'https://github.com/anantacloud/terraform-aws-kms-module.git'
+                    : 'https://github.com/anantacloud/terraform-aws-kms-module/releases'
                 }
                 target="_blank"
                 rel="noopener noreferrer"
