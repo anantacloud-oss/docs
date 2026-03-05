29ccb4d29c4e83e95a03e4cf03e93926ceb540d6
ishita-anatacloud <ishita.kapoor@anantacloud.com>
2026-03-05 11:59:22 +0530

    Create sidebaridentiq.js

---
+++ sidebaridentiq.js
@@ -0,0 +1,20 @@
+module.exports = {
+  identiqSidebar: [
+    {
+      type: 'html',
+      value: '<strong>Getting started</strong>',
+      defaultStyle: true,
+    },
+    {
+      type: 'category',
+      label: 'IdentiQ',
+      collapsible: true,
+      collapsed: false,
+      items: [
+        'overview',
+        'how-it-works',
+        'key-capabilities',
+      ],
+    },
+  ],
+};
