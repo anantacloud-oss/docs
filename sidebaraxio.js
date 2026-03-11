5775cf3180bed5a2563058383c875a84ac71a89b
ishita-anatacloud <ishita.kapoor@anantacloud.com>
2026-03-11 12:40:28 +0530

    Create sidebaraxio.js

---
+++ sidebaraxio.js
@@ -0,0 +1,20 @@
+module.exports = {
+  axioSidebar: [
+    {
+      type: 'html',
+      value: '<strong>Getting started</strong>',
+      defaultStyle: true,
+    },
+    {
+      type: 'category',
+      label: 'Axio',
+      collapsible: true,
+      collapsed: false,
+      items: [
+        'overview',
+        'how-it-works',
+        'key-capabilities',
+      ],
+    },
+  ],
+};
