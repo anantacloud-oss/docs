112bd56dc24a93985de4f6f9e9dc9c51ecfb5de1
ishita-anatacloud <ishita.kapoor@anantacloud.com>
2026-03-11 17:46:58 +0530

    Update tools.jsx

--- src/pages/tools.jsx
+++ src/pages/tools.jsx
@@ -66,8 +66,7 @@ export default function ToolsPage() {
             <img src="/img/axio.png" alt="Axio" className={styles.axioLogo}/>
             <h3>Axio</h3>
             <p>
-              Axio is an identity and authentication platform that provides centralized user authentication, identity management, 
-              and secure access control for applications and services.
+             Axio is a platform that simplifies Terraform infrastructure management by enabling teams to import modules, configure variables, and deploy infrastructure through an intuitive self-service web interface.
             </p>          
           </div>
          
